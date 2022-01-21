941f510178199d30fd541b12b304309f7d97e769
Genne23v <59734889+Genne23v@users.noreply.github.com>
2022-01-20 20:12:58 -0500

    Find my location on current map

---
+++ Find-My-Location/WhereAmI/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Device.Location;
+
+namespace WhereAmI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Starting GeoCoordinate Watcher...");
+
+            var locationWatcher = new GeoCoordinateWatcher();
+            locationWatcher.StatusChanged += (s, e) => 
+            {
+                Console.WriteLine($"GeoCoordinateWatcher:StatusChanged:{e.Status}");
+            };
+
+            locationWatcher.PositionChanged += (s, e) => 
+            {
+                locationWatcher.Stop();
+
+                Console.WriteLine($"GeoCoordinateWatcher:PositionChanged:{e.Position.Location}");
+
+                MapImage.Show(e.Position.Location);
+            };
+
+            locationWatcher.MovementThreshold = 100;
+            
+            locationWatcher.Start();
+
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
